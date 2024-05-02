8e998a8b98488b6ee6701939ca6e340cab3436f6
root <root@DESKTOP-OU8VA5U>
2024-05-02 14:53:49 -0600

    Random Tree update

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -74,6 +74,8 @@ def go(args):
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
+    #Fitting the pipeline
+    sk_pipe.fit(X_train, y_train)
     ######################################
 
     # Compute r2 and MAE
